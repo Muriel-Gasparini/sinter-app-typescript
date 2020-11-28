c38a303eff9dfc7f3609f61ed162b1bf8148ebee
Muriel Gasparini <muriel.developer@gmail.com>
2020-11-28 16:37:37 -0300

    test: should CreateAdminAccount return 400 if invalid body is provided

--- src/presentation/controllers/create-admin-account.spec.ts
+++ src/presentation/controllers/create-admin-account.spec.ts
@@ -11,6 +11,18 @@ describe('Create Admin Account', () => {
       body: {}
     }
     const response = sut.handle(httpRequest)
-    expect(response.status).toEqual(400)
+    expect(response.status).toBe(400)
+  })
+
+  test('Should return 400 if invalid body is provided', () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password'
+      }
+    }
+    const response = sut.handle(httpRequest)
+    expect(response.status).toBe(400)
   })
 })
