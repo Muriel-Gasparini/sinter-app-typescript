2321ed236da628fc1736184d25cedf736c31560a
Muriel Gasparini <muriel.developer@gmail.com>
2020-11-28 16:37:05 -0300

    feat: should CreateAdminAccount return 400 if invalid body is provided

--- src/presentation/controllers/create-admin-account.ts
+++ src/presentation/controllers/create-admin-account.ts
@@ -2,6 +2,12 @@ import { httpRequest, httpResponse } from '../protocols'
 
 export class CreateAdminAccount {
   handle (request: httpRequest): httpResponse {
+    const requiredInputs = ['name', 'password', 'key']
+
+    for (const input of requiredInputs) {
+      if (!request.body[input]) return { status: 400 }
+    }
+
     if (Object.keys(request.body).length === 0) {
       return {
         status: 400
