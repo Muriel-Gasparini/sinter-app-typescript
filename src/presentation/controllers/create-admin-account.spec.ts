df802ec3f55074dd89ab8e3287e7dc4c93ab38ba
Muriel Gasparini <muriel.developer@gmail.com>
2020-11-28 17:51:45 -0300

    test: make sure env checker return 400 if invalid key is provided

--- src/presentation/controllers/create-admin-account.spec.ts
+++ src/presentation/controllers/create-admin-account.spec.ts
@@ -1,12 +1,34 @@
 import { CreateAdminAccount } from './create-admin-account'
+import { envChecker, responseEnvChecker } from '../protocols'
 
-const makeSut = (): CreateAdminAccount => {
-  return new CreateAdminAccount()
+const makeEnvChecker = (): envChecker => {
+  class EnvCheckerStub implements envChecker {
+    check (key: string): responseEnvChecker {
+      return {
+        isError: false
+      }
+    }
+  }
+  return new EnvCheckerStub()
+}
+
+interface sutTypes {
+  sut: CreateAdminAccount
+  envCheckerStub: envChecker
+}
+
+const makeSut = (): sutTypes => {
+  const envCheckerStub = makeEnvChecker()
+  const sut = new CreateAdminAccount(envCheckerStub)
+  return {
+    sut,
+    envCheckerStub
+  }
 }
 
 describe('Create Admin Account', () => {
   test('Should return 400 if empty body is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {}
     }
@@ -16,7 +38,7 @@ describe('Create Admin Account', () => {
   })
 
   test('Should return 400 if invalid body is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -26,5 +48,20 @@ describe('Create Admin Account', () => {
     const response = sut.handle(httpRequest)
     expect(response.status).toBe(400)
     expect(response.body).toBe('Make sure you have sent all the required fields')
+  })
+
+  test('Make sure envChecker return 400 if invalid key is provided', () => {
+    const { sut, envCheckerStub } = makeSut()
+    jest.spyOn(envCheckerStub, 'check').mockReturnValueOnce({ isError: true, message: 'The provided key is invalid' })
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        key: 'invalid_key'
+      }
+    }
+    const response = sut.handle(httpRequest)
+    expect(response.status).toBe(400)
+    expect(response.body).toBe('The provided key is invalid')
   })
 })
