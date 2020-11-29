98119be8ce6eeb9638e9bec7f3cd37ac416031fc
Muriel Gasparini <muriel.developer@gmail.com>
2020-11-29 12:22:13 -0300

    test: ensure that the passed password is encrypted

---
+++ src/infra/encrypter/bcrypt.spec.ts
@@ -0,0 +1,10 @@
+import { BcryptAdapter } from './bcrypt'
+
+describe('Bcrypt Adapter', () => {
+  test('Ensure that the passed password is encrypted ', async () => {
+    const sut = new BcryptAdapter()
+    const password = 'any_password'
+    const hashedPassword = await sut.crypt(password)
+    expect(password).not.toBe(hashedPassword)
+  })
+})
