6e62ae2bbd98aa040136cc18cabedd99a5fd7b5e
Muriel Gasparini <muriel.developer@gmail.com>
2020-12-05 13:21:03 -0300

    feat: connect with mongodb and server

---
+++ src/main/server.ts
@@ -0,0 +1,9 @@
+import MongoHelper from '../infra/database/helpers/mongo-helper'
+import env from './config/env'
+
+MongoHelper.connectToMongo('')
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => { console.warn(`Server On Port ${env.port}`) })
+  })
+  .catch((err) => { console.error(err) })
