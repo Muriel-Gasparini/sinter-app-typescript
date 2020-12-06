3485fc3894850242b6a2fb7d67510563f1825bdc
Muriel Gasparini <muriel.developer@gmail.com>
2020-12-06 20:07:17 -0300

    fix: fix problems in code

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,7 @@
+import { config } from 'dotenv'
+
+config()
+
 export default {
   mongoUrl: process.env.MONGO_URL,
   port: process.env.PORT || 3000,
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,7 @@
 import MongoHelper from '../infra/database/helpers/mongo-helper'
 import env from './config/env'
 
-MongoHelper.connectToMongo('')
+MongoHelper.connectToMongo(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/app')).default
     app.listen(env.port, () => { console.warn(`Server On Port ${env.port}`) })
